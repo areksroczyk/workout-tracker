e7e3a8bed38ff50ac1bc12a21327ead61a961bb3
arc4dev <sroczyk.arkadiusz@gmail.com>
2026-04-24 17:42:15 +0200

    Initial Commit

---
+++ workout-trackerTests/workout_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  workout_trackerTests.swift
+//  workout-trackerTests
+//
+//  Created by Arkadiusz Sroczyk on 24/04/2026.
+//
+
+import Testing
+@testable import workout_tracker
+
+struct workout_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
